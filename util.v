ade7aef70c9d98b7a792103b4b90948eda3e4714
Ethan James <ethanjamesauto@gmail.com>
2022-08-15 22:05:39 -0400

    added lineto and lineto test

--- util.v
+++ util.v
@@ -1,5 +1,6 @@
 //Original author: Trammell Hudson (https://trmm.net/)
 //clk_pin = clk / 2
+//TODO: make CS only high for one clock cycle, and maybe make the whole thing 2x as fast
 module mcp4922(
 	input clk,
 	input reset,
@@ -51,4 +52,102 @@ module mcp4922(
 		end
 	end
 
+endmodule
+
+//Original author: Trammell Hudson (https://trmm.net/)
+/**
+ * Bresenham's Line Drawing algorithm.
+ * Generate straight lines from the current point to the
+ * destination.  x_in/y_in can be reset at any time with the strobe.
+ */
+module lineto(
+	input clk,
+	input reset,
+
+	input strobe,
+	input [BITS-1:0] x_in,
+	input [BITS-1:0] y_in,
+
+	output ready, // set when done
+	output axis, // did the x or y value change
+	output [BITS-1:0] x_out,
+	output [BITS-1:0] y_out
+);
+	parameter BITS = 12;
+
+	reg [BITS-1:0] err;
+
+	reg [BITS-1:0] x_dst;
+	reg [BITS-1:0] y_dst;
+
+	reg [BITS-1:0] x_out;
+	reg [BITS-1:0] y_out;
+
+	// once the output reaches the dstination, it is ready
+	// for a new point.
+	assign ready = (x_dst == x_out) && (y_dst == y_out);
+
+	reg axis;
+	reg sx;
+	reg sy;
+
+	reg signed [BITS-1:0] dx;
+	reg signed [BITS-1:0] dy;
+
+	wire signed [BITS:0] err2 = err << 1;
+
+	always @(posedge clk)
+	begin
+		//$monitor("%d %d %d %d %d", x_out, y_out, dx, dy, err);
+		if (reset) begin
+			// reset will latch the current inputs
+			x_dst <= x_in;
+			y_dst <= y_in;
+			x_out <= x_in;
+			y_out <= y_in;
+			err <= 0;
+			axis <= 0;
+		end else
+		if (strobe) begin
+			x_dst <= x_in;
+			y_dst <= y_in;
+			axis <= 0;
+
+			if (x_in > x_out) begin
+				sx <= 1;
+				dx <= x_in - x_out;
+			end else begin
+				sx <= 0;
+				dx <= x_out - x_in;
+			end
+
+			if (y_in > y_out) begin
+				sy <= 1;
+				dy <= y_in - y_out;
+			end else begin
+				sy <= 0;
+				dy <= y_out - y_in;
+			end
+
+			err <= ((x_in > x_out) ? (x_in - x_out) : (x_out - x_in))
+				-
+				((y_in > y_out) ? (y_in - y_out) : (y_out - y_in));
+
+		end else
+		if (!ready) begin
+			// move towards the dstination point
+			if (err2 > -dy)
+			begin
+				err <= err - dy;
+				x_out <= x_out + (sx ? 1 : -1);
+				axis <= 0;
+			end else
+			if (err2 < dx)
+			begin
+				err <= err + dx;
+				y_out <= y_out + (sy ? 1 : -1);
+				axis <= 1;
+			end
+		end
+	end
 endmodule
--- vectorfpga.v
+++ vectorfpga.v
@@ -6,7 +6,6 @@ module vectorfpga(
 	reg axis = 0; //selects the dac channel (2 channels)
 	reg strobe = 0; //triggers the dac
 	wire ready;
-
 	mcp4922 dac0(
 		.clk(clk),
 		.reset(reset),
@@ -17,6 +16,26 @@ module vectorfpga(
 		.ready(ready)
 	);
 
+	
+	reg line_strobe = 0;
+	reg [11:0] x = 0;
+	reg [11:0] y = 0;
+
+	wire line_ready;
+	//wire line_axis;
+	//wire [11:0] x_out;
+	//wire [11:0] y_out;
+
+	lineto line_gen(
+		.clk(clk),
+		.reset(reset),
+		.strobe(line_strobe),
+		.x_in(x),
+		.y_in(y),
+
+		.ready(line_ready)
+	);
+
 	always@(posedge clk) begin
 		//see if the dac is ready and hasn't already been triggered
 		if(ready && !strobe) begin 
@@ -24,6 +43,16 @@ module vectorfpga(
 			strobe <= 1;
 		end else begin //otherwise, make sure the dac can't be trigged again
 			strobe <= 0;
+		end
+	end
+
+	always@(posedge clk) begin
+		if(line_ready && !line_strobe) begin
+			line_strobe <= 1;
+			x <= 12'd15;
+			y <= 12'd10;
+		end else begin
+			line_strobe <= 0;
 		end
 	end
 
