e5cefdd9aed07b441e1c818651694d39a2fbc39e
Ethan James <ethanjamesauto@vt.edu>
2022-08-27 10:34:07 -0400

    wip - added code to detect beginning and end of serial transmission (doesn't work)

--- verilog/vectorfpga.v
+++ verilog/vectorfpga.v
@@ -3,7 +3,8 @@ module vectorfpga(
 
 	//UART input 
 	input rx,
-	output test,
+	output rx_avail,
+	output reg test,
 	output [7:0] rx_data,
 	
 	//DAC outputs
@@ -45,13 +46,50 @@ module vectorfpga(
 	uart_rx uart(
 		.i_Clock(clk),
 		.i_Rx_Serial(rx),
-		.o_Rx_DV(test),
+		.o_Rx_DV(rx_avail),
 		.o_Rx_Byte(rx_data)
 	);
 
-	parameter size = 4095;
+	reg [7:0] byte = 0;
+	reg state = 0;
+	parameter WAITING = 0;
+	parameter DRAWING = 1;
 
-	reg [1:0] state = 0;
+	reg [31:0] point_read = 0;
+	reg [1:0] point_offset = 0;
+
+	reg [31:0] point = 0;
+	reg point_drawn = 0;
+
+	always@(posedge rx_avail) begin
+		if (state == WAITING) begin	
+			test <= 0;
+			if (rx_data != 0) begin
+				state = DRAWING;
+			end
+		end
+		if (state == DRAWING) begin
+			test <= 1;
+			point_read[7:0] <= rx_data;
+			point_read <= point_read << 8;
+			//if (rx_data == 8'b1) begin
+			//	state = WAITING;
+			//end 
+			if (point_offset == 3) begin //we received an entire point
+				//do shit with the point
+				if (point_read == 32'h01010101) begin //we recieved the "done" command
+					state = WAITING;
+				end else begin
+					point <= point_read;
+				end
+				point_drawn <= 0;
+				point_offset <= 0;
+			end else begin
+				point_offset <= point_offset + 1;
+			end//*/
+		end
+	end
+
 	always@(posedge clk) begin
 		if (reset) begin
 			draw <= 0;
@@ -59,24 +97,7 @@ module vectorfpga(
 			x <= 0;
 			y <= 0;
 		end else if (ready) begin
-			if (state == 0) begin
-				x <= size;
-				y <= 400;
-				jump <= 1;
-			end else if (state == 1) begin
-				x <= 0;
-				y <= size - 400;
-				draw <= 1;
-			end else if (state == 2) begin
-				x <= size;
-				y <= size;
-				draw <= 1;
-			end else begin
-				x <= 0;
-				y <= 0;
-				draw <= 1;
-			end
-			state <= state + 1;
+			
 		end
 		if (jump) begin
 			jump <= 0;
